6e153e0eac845e728d299b984dc9a449be3b160d
Akram Faisal <akram.socials@gmail.com>
2024-03-03 22:57:40 +0800

    First commit!!

---
+++ src/expense_income_tracker/Expense_Income_Tracker.java
@@ -0,0 +1,9 @@
+package expense_income_tracker;
+
+public class Expense_Income_Tracker {
+    public static void main(String[] args) {
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+        
+    }
+
+}
